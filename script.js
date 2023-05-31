bbce93720fbcdffa1928026e86ac91c04e769990
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-31 17:38:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const text = document.getElementById("text");
-const delay = document.getElementById("delay");
-const btn = document.getElementById("btn");
-const output = document.getElementById("output");
+// Get elements from the HTML
+const textInput = document.getElementById('text');
+const delayInput = document.getElementById('delay');
+const outputDiv = document.getElementById('output');
+const btn = document.getElementById('btn');
 
-//your code here
-async function showMessage() {
-  const message = text.value;
-  const delayVal = delay.value;
-  await new Promise((resolve) => setTimeout(resolve, delayVal));
-  output.innerText = message;
+// Add click event listener to the button
+btn.addEventListener('click', showMessage);
+
+// Async function to wait for a specified time before resolving with a message
+async function delayedMessage(message, delay) {
+  await new Promise(resolve => setTimeout(resolve, delay));
+  return message;
 }
 
-btn.addEventListener("click", showMessage);
+// Function to show the message on the webpage after a delay
+function showMessage() {
+  const message = textInput.value;
+  const delay = Number(delayInput.value);
+
+  if (message && delay) {
+    delayedMessage(message, delay)
+      .then(result => {
+        outputDiv.innerText = result;
+      });
+  } else {
+    outputDiv.innerText = 'Please enter both message and delay.';
+  }
+}
